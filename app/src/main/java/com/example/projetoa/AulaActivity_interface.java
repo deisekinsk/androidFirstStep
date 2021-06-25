24c9b7504967792ed06f24211d33dcbc125338fa
deisesilva <kinsk@msn.com>
2021-06-25 10:51:47 -0300

    New activity from interface

---
+++ app/src/main/java/com/example/projetoa/AulaActivity_interface.java
@@ -0,0 +1,14 @@
+package com.example.projetoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AulaActivity_interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula_interface);
+    }
+}
