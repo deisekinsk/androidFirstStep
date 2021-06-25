2c2b8cd3ab77147edfdc2869893a8e5bb31e1a84
deisesilva <kinsk@msn.com>
2021-06-25 10:44:18 -0300

    Class activity

---
+++ app/src/main/java/com/example/projetoa/AulaActivity.java
@@ -0,0 +1,12 @@
+package com.example.projetoa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AulaActivity extends Activity {
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
