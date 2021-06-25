632f4ca76073dca7d693f1784890625af58411d7
deisesilva <kinsk@msn.com>
2021-06-25 11:33:49 -0300

    method onSaveInstanceState

--- app/src/main/java/com/example/projetoa/AulaActivity.java
+++ app/src/main/java/com/example/projetoa/AulaActivity.java
@@ -3,10 +3,29 @@ package com.example.projetoa;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+
 public class AulaActivity extends Activity {
+    private int dateOfDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+    private final calendarA calendarA= new calendarA();
+
+
     @Override
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (saveInstanceState != null) dateOfDay = saveInstanceState("dateOfDay");
+        calendarA.showDay(dateOfDay);
+    }
+
+    private int saveInstanceState(String dateOfDay) {
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("dateOfDay", dateOfDay);
+
     }
 }
